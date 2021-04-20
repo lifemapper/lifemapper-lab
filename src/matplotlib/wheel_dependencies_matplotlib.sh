b2041cf541a9004b650a674d6214a2d84a7b3eeb
Aimee Stewart <zzeppozz@gmail.com>
2021-04-20 17:10:53 -0500

    update

---
+++ src/matplotlib/wheel_dependencies_matplotlib.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#
+# This script defines the order of dependency installation for the current python wheel 
+
+MATPLOTLIB_DEPS="six cycler kiwisolver Pillow python-dateutil pyparsing matplotlib"
